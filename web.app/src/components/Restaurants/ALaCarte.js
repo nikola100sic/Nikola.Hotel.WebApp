470b1336f125c8abb4fb333c86d8f5cbd1f272a9
nikola100sic <nikola.stosic100@gmail.com>
2024-09-04 00:39:20 +0200

    Completed A La Carte component

--- web.app/src/components/Restaurants/ALaCarte.js
+++ web.app/src/components/Restaurants/ALaCarte.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import {Col, Row } from 'react-bootstrap';
 import image1 from '../../assets/images/alacarte.jpg'
 import image2 from '../../assets/images/alacarte2.jpg'
+import { Modal, Button } from "react-bootstrap";
+
+
+
 
 
 export const ALaCarte = () => {
+
+    const [show, setShow] = useState(false);
+    const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+    const handleClose = () => setShow(false);
+    const handleShow = (photo) => {
+        setSelectedPhoto(photo);
+        setShow(true);
+    };
+
     return (
         <div className="containerImage">
             <div className="fullscreen-backgroundImage"></div>
             <div className="content-container">
                 <Row>
-                    {/* Tekst sa leve strane */}
                     <Col md={6}>
                         <div className="text-container">
                             <h1>A La Carte Restaurant</h1>
@@ -22,16 +35,41 @@ export const ALaCarte = () => {
                         <div className='text-container'>
                             <h4>Work time</h4>
                             <p>Every day from 11am to 10pm</p>
+                            <i>Welcome!</i>
                         </div>
                     </Col>
                     
                     <Col md={6}>
                         <div className="restaurants-photo">
-                            <img src={image1} alt="Dish 1" className="restaurant-photo" />
-                            <img src={image2} alt="Dish 2" className="restaurant-photo" />
+                            <img src={image1} 
+                            alt="Photo1"
+                            className="restaurant-photo" 
+                            onClick={() => handleShow(image1)}/>
+                            <img src={image2} 
+                            alt="Photo2" 
+                            className="restaurant-photo"
+                            onClick={() => handleShow(image2)} />
                         </div>
                     </Col>
                 </Row>
+
+                <Modal show={show} onHide={handleClose} size="lg">
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <Modal.Body>
+                    <img
+                        src={selectedPhoto}
+                        alt="Selected"
+                        className="img-fluid"
+                        style={{ maxHeight: '75vh' }}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             </div>
         </div>
     );
